8fff3f7c9724459a8e3d509a3b69e684386bdf45
bzoon <ben4808@gmail.com>
2025-10-26 00:36:04 -0300

    add certs

--- src/server.ts
+++ src/server.ts
@@ -8,12 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-/*
 app.use(cors({
-    origin: ["http://localhost:3000", "https://blockquarry.net"],
+    origin: ["http://localhost:3000", "https://blockquarry.net", "http://localhost:5002", "http://localhost:5173"],
     credentials: true,
 }));
-*/
 
 // Add APIs
 app.use('/api', BaseRouter);
